d079d538ea2043b0ea19e6534bd4070fb0c2c602
MafeCercado <83511886+MafeCercado@users.noreply.github.com>
2023-10-19 05:26:15 -0500

    Segunda entrega update 4.0

--- app/src/main/java/com/example/proyecto/PerfilActivity.kt
+++ app/src/main/java/com/example/proyecto/PerfilActivity.kt
@@ -1,12 +1,64 @@
 package com.example.proyecto
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class PerfilActivity : AppCompatActivity() {
+
+    private val CAMERA_PERMISSION_REQUEST = 123
+    private val CAMERA_REQUEST = 124
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
 
+        imageView = findViewById(R.id.persona)
+
+        imageView.setOnClickListener {
+            checkCameraPermission()
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST)
+        } else {
+            openCamera()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                // Permiso de cámara denegado
+                Toast.makeText(this, "Permiso de cámara denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
     }
 }
--- app/src/main/java/com/example/proyecto/RutaActivity.kt
+++ app/src/main/java/com/example/proyecto/RutaActivity.kt
@@ -1,14 +1,6 @@
-package com.example.proyecto
 
-import android.content.pm.PackageManager
-import android.os.Bundle
-import android.widget.RelativeLayout
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapView
 
-class RutaActivity : AppCompatActivity() {
+/*class RutaActivity : AppCompatActivity() {
 
     // Este método se llama cuando la actividad se crea por primera vez
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +38,105 @@ class RutaActivity : AppCompatActivity() {
         } else {
             // Permiso denegado
             Toast.makeText(this, "Permiso de ubicación precisa denegado", Toast.LENGTH_SHORT).show()
+        }
+    }
+}*/
+
+package com.example.proyecto
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+
+
+class RutaActivity : AppCompatActivity() {
+
+    private val REQUEST_LOCATION_PERMISSION = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ruta)
+
+        // Configuramos la biblioteca de mapas de Osmdroid
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        Configuration.getInstance().userAgent = "RutaApp"
+
+        // Creamos un objeto MapView
+        val mapView = findViewById<MapView>(R.id.mapView)
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
+        mapView.setMaxZoomLevel(20.0)
+        mapView.setMinZoomLevel(1.0)
+
+        // Solicitamos el permiso de ubicación exacta
+        requestLocationPermission()
+    }
+
+    private fun requestLocationPermission() {
+        // Verificamos si el permiso ya está otorgado
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Solicitamos el permiso al usuario
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        } else {
+            // El permiso ya está otorgado, obtenemos la ubicación actual
+            getDeviceLocation()
+        }
+    }
+
+    private fun getDeviceLocation() {
+        // Creamos un cliente de servicios de ubicación
+        val locationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Solicitamos la ubicación actual
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        locationClient.lastLocation.addOnSuccessListener { location ->
+            // Si la ubicación es válida, la mostramos en un mapa
+            if (location != null) {
+                // Agregamos un marcador a la ubicación actual del dispositivo
+                val marker = Marker(mapView.projection.toScreenLocation(location.toLatLng()))
+                marker.position = location.toLatLng()
+                marker.title = "Ubicación actual"
+                mapView.addMarker(marker)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // Si el permiso fue otorgado, obtenemos la ubicación actual
+        if (requestCode == REQUEST_LOCATION_PERMISSION && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getDeviceLocation()
+        } else {
+            // El permiso fue denegado, mostramos un mensaje al usuario
+            Toast.makeText(this, "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show()
         }
     }
 }
